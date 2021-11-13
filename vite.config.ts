a50125cb20275efcb90aa4d10b39d4ddb31d3bc7
yrnana <nyryn0945@gmail.com>
2021-11-13 14:24:03 +0900

    fix: deploy 옵션 및 yml 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/tensorflow-model-example/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,4 +16,4 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-});
+}));
